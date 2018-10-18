0fcbeb77f16c040e06490b7a577ff91b6f8218cc
Brian Maher <brian.maher@kcl.ac.uk>
2018-10-18 11:36:38 +0000

    Image viewer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,37 @@
+// Enables openseadragon viewers
+function init_openseadragon()
+{
+    $('.osd-viewer').each(function()
+    {
+        var id = $(this).attr('id');
+        var url = $(this).attr('data-image-url');
+
+        OpenSeadragon({
+        id:                 id,
+        prefixUrl:          "/static/js/openseadragon/images/",
+        preserveViewport:   true,
+        visibilityRatio:    1,
+        minZoomLevel:       1,
+        defaultZoomLevel:   1,
+        sequenceMode:       false,
+        tileSources:   [{
+          "@context": "http://iiif.io/api/image/2/context.json",
+          "@id": url,
+          "height": 7200,
+          "width": 5233,
+          "profile": [ "http://iiif.io/api/image/2/level2.json" ],
+          "protocol": "http://iiif.io/api/image",
+          "tiles": [{
+            "scaleFactors": [ 1, 2, 4, 8, 16, 32 ],
+            "width": 256
+          }]
+        }]
+    });
+    });
+    
+
+}
+
 $(function() {
-    // Your JS Here
+    init_openseadragon();
 });
